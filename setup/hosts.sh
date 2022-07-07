c59a78b0d37c0636f19fc59f9b36c8f6b3321822
Kris <31840452+krisnieh@users.noreply.github.com>
2022-07-08 02:29:23 +0800

    init

--- setup/hosts.sh
+++
@@ -1,9 +0,0 @@
-# /bin/sh
-
-cat >> /etc/hosts <<EOL
-10.211.55.10 server1
-10.211.55.11 server2
-10.211.55.12 server3
-10.211.55.100 worker1
-10.211.55.101 worker2
-EOL
---
+++ setup/init.sh
@@ -0,0 +1,20 @@
+# /bin/sh
+
+cat >> /etc/hosts <<EOL
+
+192.168.8.10 server1
+192.168.8.11 server2
+192.168.8.12 server3
+192.168.8.100 worker1
+192.168.8.101 worker2
+
+EOL
+
+mkdir -p /etc/rancher/rke2/
+
+cat > /etc/rancher/rke2/config.yaml  <<EOL
+server: https://server1:9345
+token: 
+tls-san:
+    - server1
+EOL
